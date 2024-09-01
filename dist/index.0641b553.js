d34a825b3d9c7a1aeb86767ec84bcce767918e17
Shishir-Karki <sk4lama@gmail.com>
2024-09-01 19:01:53 +0530

    ii

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -3096,7 +3096,7 @@ $RefreshReg$(_c, "AppLayout");
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","react-dom/client":"lOjBx","./components/Header":"hsJbF","./components/Body":"8yaV8","./components/Footer":"8pPOA","./components/AboutUs":"47aps","./components/ContactUs":"dZ839","./components/Error":"kvula","./components/Restaurant":"kZInI","./utils/LoggedInUserContext":"asoDB","react-router-dom":"9xmpe","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru","./components/LoginRegister":"a5uW2"}],"iTorj":[function(require,module,exports) {
+},{"react/jsx-dev-runtime":"iTorj","react":"21dqq","react-dom/client":"lOjBx","./components/Header":"hsJbF","./components/Body":"8yaV8","./components/Footer":"8pPOA","./components/AboutUs":"47aps","./components/ContactUs":"dZ839","./components/Error":"kvula","./components/Restaurant":"kZInI","./utils/LoggedInUserContext":"asoDB","./components/LoginRegister":"a5uW2","react-router-dom":"9xmpe","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"km3Ru"}],"iTorj":[function(require,module,exports) {
 "use strict";
 module.exports = require("ee51401569654d91");
 
